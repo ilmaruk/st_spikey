b99957b02a65e74ab8c68cfc3c77f45edf522caf
Ruggero Ferretti <ruggero.ferretti@gmail.com>
2018-03-02 14:12:11 +0000

    Uploading from ST UI

---
+++ devicetypes/smartthings/spikey-button.src/spikey-button.groovy
@@ -0,0 +1,30 @@
+metadata {
+    definition (name: "Spikey Button", namespace: "smartthings", author: "ilmaruk") {
+        capability "Signal Strength"
+        capability "Button"
+        capability "Temperature Measurement"
+        capability "Battery"
+
+        fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0020, 0402", outClusters: "0006", manufacturer: "AlertMe.com", model: "BTN00140004", deviceJoinName: "Spikey Button"
+    }
+
+    tiles {
+        standardTile("button", "device.button", width: 2, height: 2) {
+            state("pushed", label: "Pushed", icon: "st.button.button.pushed", backgroundColor: "#00c000")
+            state("held", label: "Held", icon: "st.button.button.held", backgroundColor: "#c00000")
+        }
+    }
+}
+
+// Parse incoming device messages to generate events
+def parse(String description) {
+    log.debug "description: $description"
+}
+
+def pushed() {
+	log.debug "button pushed"
+}
+
+def held() {
+	log.debug "button held"
+}
